a30b910fb5d11e521fa1f9b25e3de638dbeba79c
ErKatta <erkatta@gmail.com>
2022-04-20 00:08:30 +0200

    3 files modified

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,8 +2,8 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { FormsModule } from '@angular/forms';
-import { MatIconModule } from '@angular/material/icon';
 import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
 import { AppComponent } from './app.component';
 import { GridComponent } from './grid/grid.component';
 
@@ -12,8 +12,8 @@ import { GridComponent } from './grid/grid.component';
     BrowserModule,
     BrowserAnimationsModule,
     FormsModule,
-    MatIconModule,
     MatToolbarModule,
+    MatIconModule,
   ],
   declarations: [AppComponent, GridComponent],
   bootstrap: [AppComponent],
