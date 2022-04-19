e778aa2a85ed2bff8f139bd9960ca508df5a0ee1
ErKatta <erkatta@gmail.com>
2022-04-19 02:02:40 +0200

    Add automatic grid sizing and click event handling

--- src/app/grid/grid.component.ts
+++ src/app/grid/grid.component.ts
@@ -1,26 +1,127 @@
-import { AfterViewInit, Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+import {
+  AfterViewInit,
+  Component,
+  ElementRef,
+  Input,
+  OnInit,
+  ViewChild,
+} from '@angular/core';
 
 @Component({
   selector: 'grid',
   templateUrl: './grid.component.html',
-  styleUrls: ['./grid.component.css']
+  styleUrls: ['./grid.component.css'],
 })
 export class GridComponent implements OnInit, AfterViewInit {
   @ViewChild('gridcanvas', { static: false })
-  gridcanvas: ElementRef<HTMLCanvasElement>; 
-  public ctx: CanvasRenderingContext2D;
+  private gridcanvas: ElementRef<HTMLCanvasElement>;
+  private ctx: CanvasRenderingContext2D;
+  @Input()
+  squareSize: number;
+  private squarePerRow: number;
+  private squarePerColumn: number;
+  private dataMatrix: Array<Array<number>>;
 
-  constructor() { }
-ngAfterViewInit(): void {
-//throw new Error('Method not implemented.');
-var ctx =  this.gridcanvas.nativeElement.getContext('2d');
-ctx.beginPath();
-ctx.arc(95, 50, 40, 0, 2 * Math.PI);
-ctx.stroke();
-ctx.closePath();
-}
+  constructor() {}
 
-  ngOnInit() {
+  ngOnInit() {}
+
+  ngAfterViewInit(): void {
+    var height = window.innerHeight;
+    var width = window.innerWidth;
+
+    // this.gridcanvas.nativeElement.setAttribute('width', String(width));
+    // this.gridcanvas.nativeElement.setAttribute('height', String(height));
+
+    this.squarePerRow = Math.floor(width / this.squareSize);
+    this.squarePerColumn = Math.floor(height / this.squareSize);
+
+    this.gridcanvas.nativeElement.setAttribute(
+      'width',
+      String(this.squarePerRow * this.squareSize)
+    );
+    this.gridcanvas.nativeElement.setAttribute(
+      'height',
+      String(this.squarePerColumn * this.squareSize)
+    );
+
+    console.log(this.squarePerRow, this.squarePerColumn);
+    this.gridcanvas.nativeElement.addEventListener('click', (e) => {
+      this.onClick(e, this.squareSize);
+    });
+    this.ctx = this.gridcanvas.nativeElement.getContext('2d');
+    this.dataMatrix = Array.from(new Array(this.squarePerRow), () =>
+      new Array(this.squarePerColumn).fill(0)
+    );
+
+    this.drawGrid();
   }
 
+  drawGrid(): void {
+    for (var i = 0; i < this.squarePerRow + 1; i++) {
+      this.ctx.beginPath();
+      this.ctx.moveTo(i * this.squareSize, 0);
+      this.ctx.lineTo(
+        i * this.squareSize,
+        this.gridcanvas.nativeElement.height
+      );
+      this.ctx.stroke();
+      this.ctx.closePath();
+    }
+
+    for (var i = 0; i < this.squarePerColumn + 1; i++) {
+      this.ctx.beginPath();
+      this.ctx.moveTo(0, i * this.squareSize);
+      this.ctx.lineTo(this.gridcanvas.nativeElement.width, i * this.squareSize);
+      this.ctx.stroke();
+      this.ctx.closePath();
+    }
+  }
+
+  onClick(e, squareSize): void {
+    console.log(e.offsetX, e.offsetY);
+    var squareRow = Math.floor(e.offsetX / squareSize);
+    var squareColumn = Math.floor(e.offsetY / squareSize);
+    console.log(squareRow, squareColumn);
+    this.changeSquareState(squareRow, squareColumn);
+  }
+
+  changeSquareState(squareRow, squareColumn): void {
+    this.dataMatrix[squareRow][squareColumn] =
+      this.dataMatrix[squareRow][squareColumn] == 0 ? 1 : 0;
+    console.log(this.dataMatrix);
+    if (this.dataMatrix[squareRow][squareColumn] == 1) {
+      this.fillSquare(squareRow, squareColumn);
+    } else {
+      this.clearSquare(squareRow, squareColumn);
+    }
+  }
+
+  fillSquare(squareRow, squareColumn): void {
+    console.log(this.squareSize);
+    this.ctx.fillStyle = 'black';
+    this.ctx.fillRect(
+      squareRow * this.squareSize,
+      squareColumn * this.squareSize,
+      this.squareSize,
+      this.squareSize
+    );
+  }
+
+  clearSquare(squareRow, squareColumn): void {
+    console.log(this.squareSize);
+
+    this.ctx.clearRect(
+      squareRow * this.squareSize,
+      squareColumn * this.squareSize,
+      this.squareSize,
+      this.squareSize
+    );
+    this.ctx.strokeRect(
+      squareRow * this.squareSize,
+      squareColumn * this.squareSize,
+      this.squareSize,
+      this.squareSize
+    );
+  }
 }
